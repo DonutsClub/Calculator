578cf8604cc36767dcdf0c5a0dba39fbea305685
Paul Susanin <pasha.susanin15521552@mail.ru>
2021-08-08 15:00:26 +0300

    feat(): add array with random numbers and with len  (#1)

---
+++ TEST.php
@@ -0,0 +1,16 @@
+<?php
+
+$n = 5;
+
+$array = array();
+
+echo($array);
+
+for ($i = 1; $i <= $n; $i++)
+{
+    $number =  rand(0, 10000);
+    array_push($array,  $number);
+    print_r($array);
+}
+
+?>
